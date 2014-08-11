a59cf1b2b5cb4bb0e75a087974677a350ef41367
niallsull@yahoo.co.uk <niallsull@yahoo.co.uk>
2014-08-11 10:58:03 +0100

    Test Commit
    Testing actual code file commit

---
+++ RetailSystem/src/TestGit.java
@@ -0,0 +1,16 @@
+
+public class TestGit {
+
+	public TestGit() {
+		// TODO Auto-generated constructor stub
+		
+	System.out.println("Testing git");
+	
+	}
+	
+	
+	public static void mani(String []args){
+		new TestGit();
+	}
+
+}
